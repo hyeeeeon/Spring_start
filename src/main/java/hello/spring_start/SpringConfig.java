cbbe318ae6d42da2c5b43830077fb805369607a1
seung hyeon <shyeeoon@gmail.com>
2024-12-27 22:41:08 +0900

    순수 JDBC

--- src/main/java/hello/spring_start/SpringConfig.java
+++ src/main/java/hello/spring_start/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.spring_start;
 
+import hello.spring_start.repository.JdbcMemberRepository;
 import hello.spring_start.repository.MemberRepository;
 import hello.spring_start.repository.MemoryMemberRepository;
 import hello.spring_start.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+         return new JdbcMemberRepository(dataSource);
     }
 
 
